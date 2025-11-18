218b852afa2e79b55eeaacf9bf1fcbde4020b0de
Urooj-Solace <sharafathira31@gmail.com>
2025-11-18 08:47:10 +0500

    Add files via upload

---
+++ Lab2Task1.java
@@ -0,0 +1,17 @@
+public class Lab2Task1
+{ 
+      public static void main(String [] args)
+      {
+      
+            int num1;	
+	    int num2;
+            int num3;
+            int average;
+
+            num1=125;
+            num2=28;
+            num3=-25;
+            average=(num1 + num2 + num3) /3;
+            System.out.println("Average" + average);
+       }
+}
---
+++ Lab2Task2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Lab2Task2{
+     public static void main(String[] args){
+     Scanner console = new Scanner(System.in);
+
+     int length;
+     int width;
+     int area;
+     int perimeter;
+
+     System.out.println("Enter the length: ");
+     length = console.nextInt();
+
+     System.out.println("Enter the width: ");
+     width = console.nextInt();
+     perimeter = 2 * (length + width);
+     
+
+     System.out.println();
+     area = length * width;
+     System.out.println("Area = " + area);
+     System.out.println("Perimeter = " + perimeter);
+     
+   
+     }
+}
